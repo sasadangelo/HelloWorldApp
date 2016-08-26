de1484c41115e4551fd417818be613008c23e4f6
Salvatore D'Angelo <sasadangelo@gmail.com>
2016-08-26 17:18:53 +0200

    Added visualization of a bitmap on the screen

--- app/src/main/java/org/androidforfun/helloworldapp/MyActivity.java
+++ app/src/main/java/org/androidforfun/helloworldapp/MyActivity.java
@@ -1,10 +1,15 @@
 package org.androidforfun.helloworldapp;
 
+import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import org.androidforfun.framework.FileIO;
 import org.androidforfun.framework.impl.AndroidFileIO;
@@ -12,36 +17,36 @@ import org.androidforfun.framework.impl.AndroidFileIO;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class MyActivity extends AppCompatActivity {
+public class MyActivity extends Activity {
+    class AndroidFastRenderView extends View {
+        Bitmap startscreenBitmap;
+        Rect dst = new Rect();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-
-        InputStream in = null;
-        String fileName = "startscreen.png";
-        TextView textView = (TextView) this.findViewById(R.id.TextView);
-        try {
-            FileIO fileIO = new AndroidFileIO(getAssets());
-            in = fileIO.readAsset("startscreen.png");
-            Bitmap bitmap = BitmapFactory.decodeStream(in);
-
-            if (bitmap == null) {
-                textView.setText("Couldn't load bitmap from asset '" + fileName + "'");
-            } else {
-                textView.setText("Bitmap from asset '" + fileName + "' successfully loaded.");
-            }
-        } catch (IOException e) {
-            textView.setText("Couldn't load bitmap from asset '" + fileName + "'");
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        public AndroidFastRenderView(Context context) {
+            super(context);
+            InputStream inputStream = null;
+            try {
+                FileIO fileIO = new AndroidFileIO(getAssets());
+                inputStream = fileIO.readAsset("startscreen.png");
+                startscreenBitmap = BitmapFactory.decodeStream(inputStream);
+            } catch (IOException e) {
+            } finally {
+                if (inputStream != null) try { inputStream.close(); } catch (IOException e) {}
             }
         }
+
+        protected void onDraw(Canvas canvas) {
+            dst.set(0, 0, 540, 960);
+            canvas.drawBitmap(startscreenBitmap, null, dst, null);
+            invalidate();
+        }
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(new AndroidFastRenderView(this));
     }
 }
