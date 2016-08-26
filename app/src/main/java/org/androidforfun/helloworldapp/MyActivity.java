e2077831f1761c3b766e1101d8a274633fcdfcad
Salvatore D'Angelo <sasadangelo@gmail.com>
2016-08-26 17:27:46 +0200

    Added visualization of a bitmap on the screen using the more performance SurfaceView class and UI Thread.

--- app/src/main/java/org/androidforfun/helloworldapp/MyActivity.java
+++ app/src/main/java/org/androidforfun/helloworldapp/MyActivity.java
@@ -2,12 +2,14 @@ package org.androidforfun.helloworldapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.view.View;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -18,12 +20,19 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class MyActivity extends Activity {
-    class AndroidFastRenderView extends View {
+    AndroidFastRenderView renderView;
+
+    public class AndroidFastRenderView extends SurfaceView implements Runnable {
         Bitmap startscreenBitmap;
         Rect dst = new Rect();
+        Thread renderThread = null;
+        SurfaceHolder holder;
+        volatile boolean running = false;
 
-        public AndroidFastRenderView(Context context) {
-            super(context);
+        public AndroidFastRenderView(Context game, Bitmap framebuffer) {
+            super(game);
+            this.holder = getHolder();
+
             InputStream inputStream = null;
             try {
                 FileIO fileIO = new AndroidFileIO(getAssets());
@@ -35,18 +44,65 @@ public class MyActivity extends Activity {
             }
         }
 
-        protected void onDraw(Canvas canvas) {
-            dst.set(0, 0, 540, 960);
-            canvas.drawBitmap(startscreenBitmap, null, dst, null);
-            invalidate();
+        public void resume() {
+            running = true;
+            renderThread = new Thread(this);
+            renderThread.start();
+        }
+
+        public void run() {
+            Rect dstRect = new Rect();
+            while(running) {
+                if(!holder.getSurface().isValid())
+                    continue;
+
+                Canvas canvas = holder.lockCanvas();
+                canvas.getClipBounds(dstRect);
+                canvas.drawBitmap(startscreenBitmap, null, dstRect, null);
+                holder.unlockCanvasAndPost(canvas);
+            }
+        }
+
+        public void pause() {
+            running = false;
+            while(true) {
+                try {
+                    renderThread.join();
+                    break;
+                } catch (InterruptedException e) {
+                    // retry
+                }
+            }
         }
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(new AndroidFastRenderView(this));
+
+        boolean isLandscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        int frameBufferWidth = isLandscape ? 480 : 320;
+        int frameBufferHeight = isLandscape ? 320 : 480;
+        Bitmap frameBuffer = Bitmap.createBitmap(frameBufferWidth,
+                frameBufferHeight, Bitmap.Config.RGB_565);
+
+        renderView = new AndroidFastRenderView(this, frameBuffer);
+
+        setContentView(renderView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        renderView.resume();
+    }
+
+    public void onPause() {
+        super.onPause();
+        renderView.pause();
     }
 }
